0fbc813e26b49afb207b7486217cabe97922eb61
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-11-08 18:16:56 +0000

    fixes unique_ptr destroyer

--- src/unique_ptr.c
+++ src/unique_ptr.c
@@ -7,41 +7,50 @@
 #include <stdlib.h>
 
 /** Lib Includes */
-#include <smart_ptr/unique_ptr.h>
+#include <smemory/unique_ptr.h>
 
 
 /** Constructs a new unique_ptr */
 unique_ptr_t *unique_ptr_make(void *ptr, destructor_t destructor) 
 {
-      unique_ptr_t *unique_ptr = malloc(sizeof(unique_ptr_t));
-      unique_ptr->ptr = ptr;
-      unique_ptr->destructor = destructor;
-      return unique_ptr;
+      unique_ptr_t *_unique_ptr = malloc(sizeof(unique_ptr_t));
+      _unique_ptr->ptr = ptr;
+      _unique_ptr->destructor = destructor;
+      return _unique_ptr;
 }
 
 
 /** Moves ownership of a unique_ptr to another unique_ptr */
-unique_ptr_t *unique_ptr_move(unique_ptr_t *source)
+unique_ptr_t *unique_ptr_move(unique_ptr_t **source)
 {
       if (source == NULL) return NULL;
+      if (*source == NULL) return NULL;
 
-      unique_ptr_t *unique_ptr = malloc(sizeof(unique_ptr_t));
-      if (unique_ptr == NULL) return NULL;
+      unique_ptr_t *_unique_ptr = malloc(sizeof(unique_ptr_t));
+      if (_unique_ptr == NULL) return NULL;
 
-      unique_ptr->ptr = source->ptr;
-      unique_ptr->destructor = source->destructor;
+      _unique_ptr->ptr = (*source)->ptr;
+      _unique_ptr->destructor = (*source)->destructor;
 
-      free(source);
-      return unique_ptr;
+      free(*source);
+      *source = NULL;
+
+      return _unique_ptr;
 }
 
 
 /** Destroys a unique_ptr */
-void unique_ptr_destroy(unique_ptr_t *ptr)
+void unique_ptr_destroy(unique_ptr_t **ptr)
 {
       if (ptr == NULL) return;
-      if (ptr->destructor != NULL) ptr->destructor(ptr->ptr);
-      free(ptr);
+      if (*ptr == NULL) return;
+
+      if ((*ptr)->destructor != NULL) {
+            (*ptr)->destructor((*ptr)->ptr);
+      }
+
+      free(*ptr);
+      *ptr = NULL;
 }
 
 
